9ef6931d349ae1d7217ff5003919fe9bdcc96842
Shun Kashiwa <shun.kashiwa@live.jp>
2018-05-15 22:46:14 +0900

    Add Constants file and put UserAgent string

---
+++ GreenNote/Source/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  GreenNote
+//
+//  Created by Shun Kashiwa on 2018/05/15.
+//  Copyright © 2018 Shun Kashiwa. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    private init {}
+    static let UserAgent: String = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1 Safari/605.1.15"
+}
