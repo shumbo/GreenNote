a2473e01e0abbe54477a7c403989c06a3e5389a6
Shun Kashiwa <shun.kashiwa@live.jp>
2018-05-17 14:39:36 +0900

    Smooth scroll for document list and PageType handler

--- GreenNote/Source/Scenes/main/MainViewController.swift
+++ GreenNote/Source/Scenes/main/MainViewController.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 import WebKit
+import RxSwift
 
-class MainViewController: UIViewController, WKNavigationDelegate {
+private enum PageType: String {
+    case TOP
+    case EDITOR
+    case OTHER
+}
 
-    @IBOutlet weak var webview: WKWebView!
+class MainViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
+    private let disposeBag = DisposeBag()
+    
+    private let currentPage: BehaviorSubject<PageType> = BehaviorSubject<PageType>(value: .TOP)
+
+    var webView: WKWebView!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
+    override func loadView() {
+        super.loadView()
+        let webConfiguration = WKWebViewConfiguration()
+        let userContentController = WKUserContentController()
+        var js = ""
+        let path = Bundle.main.path(forResource: "bundle", ofType: "js")
+        if let script = try? String.init(contentsOfFile: path!, encoding: .utf8) {
+            js = script
+        }
+        userContentController.addUserScript(WKUserScript(source: js, injectionTime: .atDocumentEnd, forMainFrameOnly: false))
+        userContentController.add(self, name: "PageType")
+        webConfiguration.userContentController = userContentController
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        self.view = webView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        webview.customUserAgent = Constants.UserAgent
-        webview.navigationDelegate = self
-        webview.load(URLRequest(url: URL(string: "https://app.grammarly.com")!))
+        webView.customUserAgent = Constants.UserAgent
+        webView.navigationDelegate = self
+        webView.load(URLRequest(url: URL(string: "https://app.grammarly.com")!))
+        
+        
+        currentPage.subscribe { e in
+            guard let page: PageType = e.element else { return }
+            switch page {
+            case .EDITOR:
+                self.shareButton.isEnabled = true
+                break
+            case .TOP:
+                self.shareButton.isEnabled = false
+                break
+            case .OTHER:
+                self.shareButton.isEnabled = false
+                break
+            }
+        }.disposed(by: self.disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,4 +79,15 @@ class MainViewController: UIViewController, WKNavigationDelegate {
         }
     }
     
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        switch message.name {
+        case "PageType":
+            guard let str = message.body as? String else { return }
+            guard let nextType = PageType(rawValue: str) else { return }
+            self.currentPage.onNext(nextType)
+            break
+        default:
+            break
+        }
+    }
 }
--- Injectee/src/index.ts
+++ Injectee/src/index.ts
@@ -1,3 +1,69 @@
-console.log('Hello, World');
-
 import './style.scss';
+
+// Interrupt existing method
+function track(fn: Function, handler: Function) {
+  return function interceptor(this: any) {
+    handler.apply(this, arguments);
+    return fn.apply(this, arguments);
+  };
+}
+
+enum PageType {
+  TOP = 'TOP',
+  EDITOR = 'EDITOR',
+  OTHER = 'OTHER'
+}
+
+class GreenNoteInjectee {
+  public currentPage: PageType = PageType.TOP;
+  constructor() {
+    window.history.pushState = track(
+      history.pushState,
+      this.handleChangeLocation
+    );
+    window.history.replaceState = track(
+      window.history.replaceState,
+      this.handleChangeLocation
+    );
+  }
+  private handleChangeLocation(state: any, title: string, url: string) {
+    if (url === '/'){
+      this.currentPage = PageType.TOP;
+    } else if (url.match(/^\/ddocs\/\d+/)) {
+      this.currentPage = PageType.EDITOR;
+    } else {
+      this.currentPage = PageType.OTHER;
+    }
+    webkit.messageHandlers.PageType.postMessage(this.currentPage);
+  }
+}
+
+(window as any)['__GreenNote__'] = new GreenNoteInjectee();
+
+// Map TouchEvent to ClickEvent
+document.addEventListener('touchstart', (e: TouchEvent) => {
+  if (e.changedTouches.length > 1) {
+    return;
+  }
+  const touch = e.changedTouches[0];
+  const eventToSimulate = document.createEvent('MouseEvent');
+  eventToSimulate.initMouseEvent(
+    'mousedown',
+    true,
+    true,
+    window,
+    1,
+    touch.screenX,
+    touch.screenY,
+    touch.clientX,
+    touch.clientY,
+    false,
+    false,
+    false,
+    false,
+    0,
+    null
+  );
+  touch.target.dispatchEvent(eventToSimulate);
+  e.preventDefault();
+}, true);
